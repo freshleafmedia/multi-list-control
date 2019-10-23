3d067f3be112426901516563009006ebba434b6f
Joe Strong <joe@freshleafmedia.co.uk>
2019-10-23 14:56:03 +0100

    Fixes code indentation style

--- src/js/List.js
+++ src/js/List.js
@@ -1,223 +1,223 @@
 export default class List {
-  constructor(element, options) {
-    this.options = Object.assign({
-      data: null
-    }, options);
-    this.wrapper = element;
-    this.listOptions = new Map();
-    this.optionListEl = null;
-    this.onOptionsChangedCallback = options.onOptionsChangedCallback || null;
-    this.onOptionRemovedCallback = options.onOptionRemovedCallback || null;
-    this.dragSource = null;
-    this.dragOverTarget = null;
-    this.dragOverDirection = 1;
-    this.dragTimeCount = 1;
-    this.dragPlaceholder = document.createElement('div');
+    constructor(element, options) {
+        this.options = Object.assign({
+            data: null
+        }, options);
+        this.wrapper = element;
+        this.listOptions = new Map();
+        this.optionListEl = null;
+        this.onOptionsChangedCallback = options.onOptionsChangedCallback || null;
+        this.onOptionRemovedCallback = options.onOptionRemovedCallback || null;
+        this.dragSource = null;
+        this.dragOverTarget = null;
+        this.dragOverDirection = 1;
+        this.dragTimeCount = 1;
+        this.dragPlaceholder = document.createElement('div');
 
-    this.initStyle();
-    this.initEvents();
-  }
+        this.initStyle();
+        this.initEvents();
+    }
 
-  initStyle() {
-    this.wrapper.className = 'MultiList__List';
+    initStyle() {
+        this.wrapper.className = 'MultiList__List';
 
-    this.optionListEl = document.createElement('ol');
-    this.wrapper.appendChild(this.optionListEl);
+        this.optionListEl = document.createElement('ol');
+        this.wrapper.appendChild(this.optionListEl);
 
-    this.dragPlaceholder.className = 'MultiList__Placeholder';
-  }
+        this.dragPlaceholder.className = 'MultiList__Placeholder';
+    }
 
-  initEvents() {
-    this.optionListEl.addEventListener('click', e => {
-      const el = e.target.closest('.MultiList__Close');
-      if (!el) {
-        return;
-      }
+    initEvents() {
+        this.optionListEl.addEventListener('click', e => {
+            const el = e.target.closest('.MultiList__Close');
+            if (!el) {
+                return;
+            }
 
-      const liEl = el.closest('li');
-      const optionId = liEl.dataset.id;
-      const option = this.listOptions.get(optionId);
+            const liEl = el.closest('li');
+            const optionId = liEl.dataset.id;
+            const option = this.listOptions.get(optionId);
 
-      this.onDeleteClick(option);
-    });
-    this.optionListEl.addEventListener('dragstart', e => {
-      const dt = e.dataTransfer;
-      dt.dropEffect = 'move';
-      try {
-        dt.setData('multilist-item', 'multilist-item');
-      } catch (e) {
-        dt.setData('text', 'multilist-item');
-      }
-      this.dragSource = e.target;
-    });
-    this.optionListEl.addEventListener('dragenter', e => {
-      e.preventDefault();
-      const el = e.target.closest('li');
-      if (el === null || el === this.dragOverTarget) {
-        return;
-      }
-
-      this.dragOverTarget = el;
-      this.dragTimeCount = 1;
-      this.positionDragPlaceholder(el, e);
-    });
-    this.optionListEl.addEventListener('dragover', e => {
-      e.preventDefault();
-      const el = e.target.closest('li');
-      this.dragTimeCount++;
-      if (el === null || this.dragTimeCount % 3 !== 0) {
-        return;
-      }
-
-      this.dragOverTarget = el;
-      this.positionDragPlaceholder(el, e);
-    });
-    this.optionListEl.addEventListener('drop', e => {
-      e.preventDefault();
-    });
-    this.optionListEl.addEventListener('dragend', e => {
-      this.dragPlaceholder.insertAdjacentElement('afterend', this.dragSource);
-      if (this.dragOverDirection === 0) {
-        this.reorderItemBefore(this.dragSource.dataset.id, this.dragOverTarget.dataset.id);
-      } else {
-        this.reorderItemAfter(this.dragSource.dataset.id, this.dragOverTarget.dataset.id);
-      }
-      this.removeDragPlaceholder();
-    });
-  }
-
-  readOptionsFromInput(input) {
-    this.listOptions.clear();
-    input.querySelectorAll('option').forEach(optionEl => {
-      if (optionEl.selected === false) {
-        return;
-      }
-
-      const newOption = { name: optionEl.textContent, id: optionEl.value };
-
-      if (this.options.data) {
-        this.options.data.forEach(dataKey => {
-          if (!optionEl.dataset[dataKey]) {
-            return;
-          }
-
-          newOption[dataKey] = optionEl.dataset[dataKey];
+            this.onDeleteClick(option);
         });
-      }
+        this.optionListEl.addEventListener('dragstart', e => {
+            const dt = e.dataTransfer;
+            dt.dropEffect = 'move';
+            try {
+                dt.setData('multilist-item', 'multilist-item');
+            } catch (e) {
+                dt.setData('text', 'multilist-item');
+            }
+            this.dragSource = e.target;
+        });
+        this.optionListEl.addEventListener('dragenter', e => {
+            e.preventDefault();
+            const el = e.target.closest('li');
+            if (el === null || el === this.dragOverTarget) {
+                return;
+            }
 
-      this.listOptions.set(newOption.id, newOption);
-    });
+            this.dragOverTarget = el;
+            this.dragTimeCount = 1;
+            this.positionDragPlaceholder(el, e);
+        });
+        this.optionListEl.addEventListener('dragover', e => {
+            e.preventDefault();
+            const el = e.target.closest('li');
+            this.dragTimeCount++;
+            if (el === null || this.dragTimeCount % 3 !== 0) {
+                return;
+            }
 
-    this.renderList();
-  }
-
-  renderList() {
-    this.optionListEl.innerHTML = '';
-    this.listOptions.forEach((option, key, map) => {
-      const optionEl = document.createElement('li');
-      optionEl.innerHTML = this.options.markup.item.call(this, option);
-      optionEl.dataset.id = option.id;
-      optionEl.draggable = true;
-      this.optionListEl.appendChild(optionEl);
-    });
-  }
-
-  add(option) {
-    this.listOptions.set(option.id.toString(), option);
-
-    this.onOptionsChanged();
-    this.renderList();
-  }
-
-  reorderItemBefore(sourceOptionId, targetOptionId) {
-    if (sourceOptionId === targetOptionId) {
-      return;
+            this.dragOverTarget = el;
+            this.positionDragPlaceholder(el, e);
+        });
+        this.optionListEl.addEventListener('drop', e => {
+            e.preventDefault();
+        });
+        this.optionListEl.addEventListener('dragend', e => {
+            this.dragPlaceholder.insertAdjacentElement('afterend', this.dragSource);
+            if (this.dragOverDirection === 0) {
+                this.reorderItemBefore(this.dragSource.dataset.id, this.dragOverTarget.dataset.id);
+            } else {
+                this.reorderItemAfter(this.dragSource.dataset.id, this.dragOverTarget.dataset.id);
+            }
+            this.removeDragPlaceholder();
+        });
     }
 
-    const newMap = new Map();
+    readOptionsFromInput(input) {
+        this.listOptions.clear();
+        input.querySelectorAll('option').forEach(optionEl => {
+            if (optionEl.selected === false) {
+                return;
+            }
 
-    this.listOptions.forEach((value, key) => {
-      if (key === sourceOptionId) {
-        return;
-      }
-      if (key === targetOptionId) {
-        newMap.set(sourceOptionId, this.listOptions.get(sourceOptionId));
-        newMap.set(key, value);
-        return;
-      }
-      newMap.set(key, value);
-    });
+            const newOption = { name: optionEl.textContent, id: optionEl.value };
 
-    this.listOptions = newMap;
+            if (this.options.data) {
+                this.options.data.forEach(dataKey => {
+                    if (!optionEl.dataset[dataKey]) {
+                        return;
+                    }
 
-    this.onOptionsChanged();
-  }
+                    newOption[dataKey] = optionEl.dataset[dataKey];
+                });
+            }
 
-  reorderItemAfter(sourceOptionId, targetOptionId) {
-    if (sourceOptionId === targetOptionId) {
-      return;
+            this.listOptions.set(newOption.id, newOption);
+        });
+
+        this.renderList();
     }
 
-    const newMap = new Map();
-
-    this.listOptions.forEach((value, key) => {
-      if (key === sourceOptionId) {
-        return;
-      }
-      if (key === targetOptionId) {
-        newMap.set(key, value);
-        newMap.set(sourceOptionId, this.listOptions.get(sourceOptionId));
-        return;
-      }
-      newMap.set(key, value);
-    });
-
-    this.listOptions = newMap;
-
-    this.onOptionsChanged();
-  }
-
-  onOptionsChanged() {
-    if (this.onOptionsChangedCallback) {
-      this.onOptionsChangedCallback.call(this, this.listOptions);
+    renderList() {
+        this.optionListEl.innerHTML = '';
+        this.listOptions.forEach((option, key, map) => {
+            const optionEl = document.createElement('li');
+            optionEl.innerHTML = this.options.markup.item.call(this, option);
+            optionEl.dataset.id = option.id;
+            optionEl.draggable = true;
+            this.optionListEl.appendChild(optionEl);
+        });
     }
-  }
 
-  onOptionRemoved(option) {
-    if (this.onOptionRemovedCallback) {
-      this.onOptionRemovedCallback.call(this, option);
+    add(option) {
+        this.listOptions.set(option.id.toString(), option);
+
+        this.onOptionsChanged();
+        this.renderList();
     }
-  }
 
-  onDeleteClick(option) {
-    this.listOptions.delete(option.id);
+    reorderItemBefore(sourceOptionId, targetOptionId) {
+        if (sourceOptionId === targetOptionId) {
+            return;
+        }
 
-    this.onOptionRemoved(option);
-    this.onOptionsChanged();
-    this.renderList();
-  }
+        const newMap = new Map();
 
-  positionDragPlaceholder(targetEl, dragEvent) {
-    const mouseY = dragEvent.offsetY;
+        this.listOptions.forEach((value, key) => {
+            if (key === sourceOptionId) {
+                return;
+            }
+            if (key === targetOptionId) {
+                newMap.set(sourceOptionId, this.listOptions.get(sourceOptionId));
+                newMap.set(key, value);
+                return;
+            }
+            newMap.set(key, value);
+        });
 
-    if (mouseY >= (targetEl.getBoundingClientRect().height / 2)) {
-      targetEl.insertAdjacentElement('afterend', this.dragPlaceholder);
-      this.dragOverDirection = 1;
-    } else {
-      targetEl.insertAdjacentElement('beforebegin', this.dragPlaceholder);
-      this.dragOverDirection = 0;
+        this.listOptions = newMap;
+
+        this.onOptionsChanged();
     }
-  }
 
-  removeDragPlaceholder() {
-    this.optionListEl.removeChild(this.dragPlaceholder);
-  }
+    reorderItemAfter(sourceOptionId, targetOptionId) {
+        if (sourceOptionId === targetOptionId) {
+            return;
+        }
 
-  /**
+        const newMap = new Map();
+
+        this.listOptions.forEach((value, key) => {
+            if (key === sourceOptionId) {
+                return;
+            }
+            if (key === targetOptionId) {
+                newMap.set(key, value);
+                newMap.set(sourceOptionId, this.listOptions.get(sourceOptionId));
+                return;
+            }
+            newMap.set(key, value);
+        });
+
+        this.listOptions = newMap;
+
+        this.onOptionsChanged();
+    }
+
+    onOptionsChanged() {
+        if (this.onOptionsChangedCallback) {
+            this.onOptionsChangedCallback.call(this, this.listOptions);
+        }
+    }
+
+    onOptionRemoved(option) {
+        if (this.onOptionRemovedCallback) {
+            this.onOptionRemovedCallback.call(this, option);
+        }
+    }
+
+    onDeleteClick(option) {
+        this.listOptions.delete(option.id);
+
+        this.onOptionRemoved(option);
+        this.onOptionsChanged();
+        this.renderList();
+    }
+
+    positionDragPlaceholder(targetEl, dragEvent) {
+        const mouseY = dragEvent.offsetY;
+
+        if (mouseY >= (targetEl.getBoundingClientRect().height / 2)) {
+            targetEl.insertAdjacentElement('afterend', this.dragPlaceholder);
+            this.dragOverDirection = 1;
+        } else {
+            targetEl.insertAdjacentElement('beforebegin', this.dragPlaceholder);
+            this.dragOverDirection = 0;
+        }
+    }
+
+    removeDragPlaceholder() {
+        this.optionListEl.removeChild(this.dragPlaceholder);
+    }
+
+    /**
    * Get all the selected options
    * @return {Map} A Map of all the options
    */
-  getOptions() {
-    return this.listOptions;
-  }
+    getOptions() {
+        return this.listOptions;
+    }
 }
